f5202c183dea71031c616d846672eb734612a18e
Radney <gordon.radney@disney.com>
2024-01-08 10:38:36 -0500

    added abstract fun example

--- src/main/kotlin/com/sonlit/kumandra/Creature.kt
+++ src/main/kotlin/com/sonlit/kumandra/Creature.kt
@@ -3,11 +3,15 @@ package com.sonlit.kumandra
 abstract class Creature(
     val kind: String
 ){
+    abstract fun birthDate(): Int
     open fun description() = println("This kind of creature is a $kind.")
 }
 
-class Animal(kind: String): Creature(kind)
+class Animal(kind: String): Creature(kind) {
+    override fun birthDate(): Int = 18000101
+}
 
 class Human: Creature(kind = "human") {
+    override fun birthDate(): Int = 19000101
     override fun description() = println("This kind of creature is a $kind and is created in the image of God.")
 }
